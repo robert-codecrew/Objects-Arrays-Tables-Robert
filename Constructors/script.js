966f8566cd55221d4c6b534093962716f99a15d6
Michael Curry <92552616+MichaelCurry99@users.noreply.github.com>
2022-06-16 14:22:05 -0500

    Create script.js

---
+++ Constructors/script.js
@@ -0,0 +1,25 @@
+Constructors -
+
+// Create a constructor of Dog. Make sure it has the attributes name, breed, color, gender. There are no methods in this class. The main function below should work with the class you create.
+
+function Dog(name, breed, color, gender) {
+    this.name = first;
+    this.breedName = last;
+    this.color = color;
+    this.gender = gender;
+  }
+
+  Dog();
+
+ //  Create a ToDo constructor. Create the attributes: name, dueDate, list (this should be an array). There should not be a method in this class. The main function below should work with the class you create.
+
+  function main(){
+	newToDoList = new ToDo("Kenn", "Next Week", ["Do the dishes", "Wash your clothes"])
+    this.name = 'Kenn';
+    this.dueDate = 'Next Week',
+    this.list = 'Wash your clothes',
+    
+	print(newToDoList.list)
+}
+
+main();
