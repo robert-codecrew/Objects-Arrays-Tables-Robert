1b651f014f473be32bee66a4a9d45866706b2757
Michael Curry <92552616+MichaelCurry99@users.noreply.github.com>
2022-06-16 13:51:48 -0500

    Update app.js

---
+++ Object-Literals/app.js
@@ -0,0 +1,37 @@
+  // Object Literals -
+
+// Jimmy Page, 62, 00821
+
+// Rick Nielsen, 57, 61016
+
+// Joe Walsh, 58, 90001
+
+// Lemmy Kilmister, 57, 21120
+
+
+// First Name Last Name Age Zip Code
+
+let person1 = {
+    name: 'Jimmmy Page',
+    age: 62,
+    zipcode: 821,
+}
+
+let person2 = {
+    name: 'Rick Nielsen',
+    age: 57,
+    zipcode: 61016,
+}
+
+let person3 = {
+    name: 'Joe Walsh',
+    age: 58,
+    zipcode: 90001,
+}
+
+let person4 = {
+    name: 'Lemmy Kilmister',
+    age:57,
+    zipcode: 21120,
+}
+
