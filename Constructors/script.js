68b7a98c5a621168ff43c53f0fff7c9dff6a3cd3
robert-codecrew <101600881+robert-codecrew@users.noreply.github.com>
2022-06-27 10:21:47 -0500

    Please review the comments I left in your code.

--- Constructors/script.js
+++ Constructors/script.js
@@ -1,25 +1,26 @@
-Constructors -
+ Constructors - // maybe try commenting this line out? -R.A.
 
 // Create a constructor of Dog. Make sure it has the attributes name, breed, color, gender. There are no methods in this class. The main function below should work with the class you create.
 
-function Dog(name, breed, color, gender) {
-    this.name = first;
-    this.breedName = last;
-    this.color = color;
+function Dog(name, breed, color, gender) {   // what does the function Dog do? -R.A.
+    this.name = first; // what is 'first'? -R.A.
+    this.breedName = last; // what is 'last' -R.A.
+     this.color = color;
     this.gender = gender;
   }
 
-  Dog();
+  Dog();      // why are you calling the Dog constructor function here? -R.A.
 
  //  Create a ToDo constructor. Create the attributes: name, dueDate, list (this should be an array). There should not be a method in this class. The main function below should work with the class you create.
 
   function main(){
-	newToDoList = new ToDo("Kenn", "Next Week", ["Do the dishes", "Wash your clothes"])
-    this.name = 'Kenn';
-    this.dueDate = 'Next Week',
-    this.list = 'Wash your clothes',
+	  
+	newToDoList = new ToDo("Kenn", "Next Week", ["Do the dishes", "Wash your clothes"]) // what is newToDoList? Where is the object constructor? -R.A.
+    this.name = 'Kenn';               //  at this point the keyword, 'this',refers to what exactly? -R.A.
+    this.dueDate = 'Next Week',       //  at this point the keyword, 'this',  refers to what exactly? -R.A.
+    this.list = 'Wash your clothes',  //  at this point the keyword, 'this',  refers to what exactly? -R.A.
     
-	print(newToDoList.list)
+	print(newToDoList.list)  // what is print in JavaScript? -R.A.
 }
 
 main();
